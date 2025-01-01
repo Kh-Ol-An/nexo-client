ab1ec25176d27137ecc9a600d1dbdea0ed93f80d
KhOlAn <olegkhristenko@gmail.com>
2025-01-01 22:14:38 +0200

    installed project with Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen pt-8 px-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl text-zinc-400 font-bold">Hello Nexo</h1>
+    </div>
+  );
+}
